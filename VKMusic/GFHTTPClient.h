45045841e4f883e26c91d7081624cfd298494743
Sergey Shpygar <shpygar@gmail.com>
2014-07-04 23:39:45 +0400

    Добавил клиент для работы с запросами

---
+++ VKMusic/GFHTTPClient.h
@@ -0,0 +1,21 @@
+//
+//  GFHTTPClient.h
+//  VKMusic
+//
+//  Created by Sergey Shpygar on 01.06.14.
+//  Copyright (c) 2013 Sergey Shpygar. All rights reserved.
+//
+
+
+#import "GFModelManager.h"
+
+
+typedef void (^GFHTTPClientCompletionBlock)(GFPlaylist *playlist, BOOL success, NSError *error);
+
+@interface GFHTTPClient : NSObject
+
++ (instancetype)sharedClient;
+
+- (void)getAudiosWithCompletion:(GFHTTPClientCompletionBlock)completion;
+
+@end
---
+++ VKMusic/GFHTTPClient.m
@@ -0,0 +1,91 @@
+//
+//  GFHTTPClient.m
+//  VKMusic
+//
+//  Created by Sergey Shpygar on 01.06.14.
+//  Copyright (c) 2013 Sergey Shpygar. All rights reserved.
+//
+
+
+#import "GFHTTPClient.h"
+#import "GFMappingManager.h"
+#import "GFModelManager.h"
+#import "VKSdk.h"
+
+
+#define kDefaultPlaylistID 666
+
+@interface GFHTTPClient ()
+
+@property (nonatomic, strong) GFMappingManager *objectsParser;
+@property (nonatomic, assign, getter = isUpdating) BOOL updating;
+
+@end
+
+@implementation GFHTTPClient
+
++ (instancetype)sharedClient
+{
+    // singleton initialization
+    
+    static dispatch_once_t pred = 0;
+    __strong static id client = nil;
+    dispatch_once(&pred, ^{
+        client = [[self alloc] init];
+    });
+    return client;
+}
+
+- (GFMappingManager *)objectsParser
+{
+    if (! _objectsParser) {
+        _objectsParser = [[GFMappingManager alloc] init];
+    }
+    return _objectsParser;
+}
+
+-(void)getAudiosWithCompletion:(GFHTTPClientCompletionBlock)completion{
+    NSString *userId = [VKSdk getAccessToken].userId;
+    VKRequest * audioReq = [VKApi requestWithMethod:@"audio.get"
+                                      andParameters:@{VK_API_OWNER_ID : userId} andHttpMethod:@"GET"];
+	[audioReq executeWithResultBlock: ^(VKResponse *response) {
+	    VKAudios *audios = [[VKAudios alloc] initWithDictionary:response.json objectClass:[VKAudio class]];
+        if (audios.items) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                GFPlaylist *playlist = nil;
+                NSManagedObjectContext *context = [GFModelManager sharedManager].managedObjectContext;
+                NSError *error = nil;
+                playlist = [context objectWithEntityName:[GFPlaylist entityName] value:@(kDefaultPlaylistID) forKey:@"playlistID"];
+                if (! playlist) {
+                    playlist = [GFPlaylist insertInManagedObjectContext:context];
+                    playlist.playlistID = @(kDefaultPlaylistID);
+                }
+                
+                [self.objectsParser importAudios:audios.items
+                                      toPlaylist:&playlist
+                                         context:context
+                                           error:&error];
+                
+                [context processPendingChanges];
+                if ([context hasChanges]) {
+                    [context save:&error];
+                }
+                if (completion) {
+                    completion(playlist, YES, nil);
+                }
+            });
+        }
+        else{
+            if (completion) {
+                completion(nil, NO, nil);
+            }
+        }
+    }
+    errorBlock: ^(NSError *error) {
+        if (completion) {
+            completion(nil, NO, error);
+        }
+    }];
+}
+
+@end
